4eeb957772f92ad6c4d34761224203803066b726
Kevbast <kevbast936@gmail.com>
2026-03-25 09:43:17 +0100

    Consumiendo Api Hospitales

---
+++ MvcCoreApiClient/Controllers/HospitalesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreApiClient.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+    }
+}
